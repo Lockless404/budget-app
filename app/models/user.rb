00ff3db38a0692169973f4d7dcb5de854f32af71
Hamza <hamzatarar8@gmail.com>
2022-05-14 23:37:50 +0200

    completed unit and integration tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+
   has_many :groups
   has_many :purchases
 end
