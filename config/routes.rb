9988b02e3b51a5c779912387d7d62cba72a8f8a3
Hamza <hamzatarar8@gmail.com>
2022-05-16 09:11:47 +0200

    added demo.

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   unauthenticated do
-    root "users#index"
+    root "users#index", as: 'landing'
   end
 
-  devise_scope :user do
-    root 'groups#index', as: 'categories'
+  authenticated :user do
+    root 'groups#index'
     resources :users 
     resources :groups, only: %i[index create new destroy] do
       resources :purchases, only: %i[index create new destroy]
